5c81636192edaff69a53bc98ad14890a9053191e
Braily G <94770717+BrailyGuzman@users.noreply.github.com>
2022-07-17 13:56:05 -0400

    Update and rename main.py to ip-lookup.py

---
+++ ip-lookup.py
@@ -0,0 +1,14 @@
+import os
+try:
+    import requests
+except ImportError:
+    os.system("pip install requests")
+    import requests
+
+os.system("cls")
+
+while True:
+    ip = input("Enter IP address: ")
+    url = f"https://ipapi.co/{ip}/json"
+    lookup = requests.get(url)
+    print(lookup.text)
